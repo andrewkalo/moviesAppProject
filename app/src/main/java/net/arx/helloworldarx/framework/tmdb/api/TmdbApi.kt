dad160c95a387a2cca0092ea9a9234e432571c9d
kastik <kostas21422112@gmail.com>
2024-01-19 16:56:17 +0200

    Add API calls a response model

--- app/src/main/java/net/arx/helloworldarx/framework/tmdb/api/TmdbApi.kt
+++ app/src/main/java/net/arx/helloworldarx/framework/tmdb/api/TmdbApi.kt
@@ -1,10 +1,16 @@
 package net.arx.helloworldarx.framework.tmdb.api
 
+import net.arx.helloworldarx.data.tmdb.datasource.TmdbDataModel
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface TmdbApi {
 
     @GET("path/here")
     suspend fun getExampleChangeName(@Path("example") example: String): String
+
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzQwYWY2YmZkNGE3OWUyZTIwMTgyMDkxNmU5MjU5NSIsInN1YiI6IjY1YTRmOTIzMWZiOTRmMDBjMDc0OThiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_-vuPeuY5kac4ycWHx7N2f1A4jXZkQp0WBIOEsh_j0")
+    @GET("/3/movie/{movieId}")
+    suspend fun fetchMovieDetails(@Path("movieId") movie: String): TmdbDataModel
 }
