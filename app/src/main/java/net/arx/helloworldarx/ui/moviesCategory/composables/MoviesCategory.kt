65c207d369f7b846623961a47596fbcc89b17163
kindi24 <kyndelerosvasilis@gmail.com>
2024-01-22 23:09:18 +0200

    Action setOnclickListener and add RecyclerView

--- app/src/main/java/net/arx/helloworldarx/ui/moviesCategory/composables/MoviesCategory.kt
+++ app/src/main/java/net/arx/helloworldarx/ui/moviesCategory/composables/MoviesCategory.kt
@@ -1,5 +1,8 @@
 package net.arx.helloworldarx.ui.moviesCategory.composables
 
+import android.content.Intent
+import android.media.Image
+import android.os.Bundle
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -12,12 +15,31 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.viewModelFactory
+import net.arx.helloworldarx.R
 import net.arx.helloworldarx.data.tmdb.datasource.TmdbDataModel
 import net.arx.helloworldarx.ui.movieDetails.MovieDetailsViewModel
+
+
+class MoviesCategory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_movies_category)
+
+        val backDash = findViewById<ImageButton>(R.id.backButton)
+        backDash.setOnClickListener {
+            // To dashBoard einai to onoma klashs tou arxeio sto dashboard.kt
+            //val Intent = Intent(this, dashBoard::class.java)
+            //startActivity(Intent)
+        }
+    }
+}
 
