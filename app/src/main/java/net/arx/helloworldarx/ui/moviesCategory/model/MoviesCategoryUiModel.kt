fcd94dbf6c2d69b9416ef4fc113ecaf987c7e393
kindi24 <kyndelerosvasilis@gmail.com>
2024-01-29 18:10:54 +0200

    Little fix in UiModel

--- app/src/main/java/net/arx/helloworldarx/ui/moviesCategory/model/MoviesCategoryUiModel.kt
+++ app/src/main/java/net/arx/helloworldarx/ui/moviesCategory/model/MoviesCategoryUiModel.kt
@@ -1,10 +1,9 @@
 package net.arx.helloworldarx.ui.moviesCategory.model
 
 sealed interface MoviesCategoryUiState {
-    class DefaultUiState(val MoviesCategoryUiType: MoviesCategoryUiType) : MoviesCategoryUiState
-    object ErrorUiState : MoviesCategoryUiState
     object LoadingUiState : MoviesCategoryUiState
-
+    data class DefaultUiState(val MoviesCategoryUiType: MoviesCategoryUiType) : MoviesCategoryUiState
+    data object ErrorUiState : MoviesCategoryUiState
     object EmptyUiState : MoviesCategoryUiState
 }
 
